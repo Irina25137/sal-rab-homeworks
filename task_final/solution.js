0458436dd01438d1a149e15ce9d899aa4e77827f
IrinaSm <allsorts@mail.ru>
2022-03-22 17:29:41 +0300

    final task

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,20 +34,35 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+    //    let data = {goods: [], order: {}};
+    let client = '';
+    let data = { 
+        client, order: {}, goods: [] 
+    };
 
     let countOfGoods = goods.length;
-
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
+    
+    for (let i = 0; i < countOfGoods; i +=1 ) {
+        // добавлено
+        let s = {};
+        s = {
+            'title': goods[i].title, 'count': goods[i].count
+        };
+        data.goods.push(s);
+        // data.goods.push(goods[i].count);
     }
+    
+    data.order.address = 'ул. ' + address.street + ', дом ' +  address.house + ', ' + address.entrance + ' подъезд, '+ address.floor +  ' этаж, кв. ' + address.flat;
+    //     data.order.sum = name + phone + address + goods + sum;
+    data.order.sum = sum;
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
+    // data.client = 'Иван';
+    data.client = name + ' ' + phone;
 
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
-
-    return jsonData;
+    let obj = {data};
+    
+    // let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify(obj);
+    
+     
 }
